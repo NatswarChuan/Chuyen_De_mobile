3c3fbc85f17aa159fe67edbcf12ee27a3977b701
NatswarChuan <natswarchuan@gmail.com>
2021-09-20 21:23:27 +0700

    20/9 upload API [updateView]

--- product-service/app/Model/ProductModel.js
+++ product-service/app/Model/ProductModel.js
@@ -85,7 +85,7 @@ function getProducts(req, res) {
                 return res.send({ status: "fail", message: 'không có sản phẩm trong cơ sở dữ liệu' });
             }
             else {
-                db.getProducts(results,option,req,res);
+                db.getProducts(results, option, req, res);
             }
         });
     }
@@ -94,7 +94,130 @@ function getProducts(req, res) {
     }
 }
 
+//tìm kiếm sản phẩm
+function searchProducts(req, res) {
+    let key = req.params.key;
+    let option = req.params.option;
+    let key_search = '%' + req.params.key_search + '%';
+    if (key == config.key) {
+        let sql = '';
+        switch (option) {
+            case '0':
+                sql = 'SELECT `product_id` FROM `product` WHERE `product_title` LIKE ? AND `status` = 1';
+                break;
+            case '1':
+                sql = 'SELECT `product_id` FROM `product` WHERE `product_title` LIKE ?';
+                break;
+            default:
+                return res.send({ status: "fail", message: 'key không hợp lệ' });
+        }
+
+
+        db.dbConn.query(sql, key_search, function (error, results, fields) {
+            if (error) throw error;
+            if (results == null || results.length === 0) {
+                return res.send({ status: "fail", message: 'không có sản phẩm trong cơ sở dữ liệu' });
+            }
+            else {
+                db.getProducts(results, option, req, res);
+            }
+        });
+    }
+    else {
+        return res.send({ status: "fail", message: 'key không hợp lệ' });
+    }
+}
+
+//cập nhật lượt xem và trả về thông tin sản phẩm
+function updateView(req, res) {
+    let key = req.params.key;
+    if (key == config.key) {
+        let cookies_view = [];
+        //kiểm tra đã tồn tại cookie chưa
+        if (req.cookies.view_product) {
+            let cookies_view = req.cookies.view_product;
+            let view_product = cookies_view.find(function (element) {
+                return element.product_id == req.params.product_id;
+            });
+
+            //kiểm tra sản phẩm đã tăng view chưa
+            if (typeof view_product == 'undefined') {
+                cookies_view.push(
+                    {
+                        product_id: req.params.product_id,
+                        expires: Number(new Date(Date.now() + 86400000)),
+                    }
+                );
+                res.cookie('view_product', cookies_view);
+                setView(req, res);
+            }
+            //kiểm tra cookie của sản phẩm đã hết hạn chưa
+            else if (view_product.expires <= Number(new Date(Date.now()))) {
+                cookies_view.find(element => element.product_id == req.params.product_id).expires = Number(new Date(Date.now() + 86400000));
+                res.cookie('view_product', cookies_view);
+                setView(req, res);
+            } else {
+                let product;
+                let id = req.params.product_id;
+                let option = req.params.option;
+                axios.get(config.app_url + `/api/product/` + id + `/` + option + `/` + req.params.key)
+                    .then(res => {
+                        const { data } = res.data;
+                        product = data[0];
+                    })
+                    .catch(error => console.log(error))
+                    .finally(() => {
+                        return res.send({ status: "fail", data: product, message: 'sản phẩm đã không cập nhật lượt xem' });
+                    })
+            }
+        }
+        else {
+            cookies_view.push(
+                {
+                    product_id: req.params.product_id,
+                    expires: Number(new Date(Date.now() + 86400000)),
+                }
+            );
+            res.cookie('view_product', cookies_view);
+            setView(req, res);
+        }
+        function setView(req, res) {
+            let id = req.params.product_id;
+            let option = req.params.option;
+            let view = 0;
+            let product;
+            axios.get(config.app_url + `/api/product/` + id + `/` + option + `/` + req.params.key)
+                .then(res => {
+                    const { data } = res.data;
+                    product = data[0];
+                    product.product_view++;
+                    db.dbConn.query('UPDATE `product` SET `product_view`= ? WHERE `product_id` = ?', [product.product_view, id], function (error, results, fields) {
+                        if (error) throw error;
+                    });
+                })
+                .catch(error => console.log(error))
+                .finally(() => {
+                    axios.get(config.app_url + `/api/product/` + id + `/` + option + `/` + req.params.key)
+                        .then(res => {
+                            const { data } = res.data;
+                            product = data[0];
+                        })
+                        .catch(error => console.log(error))
+                        .finally(() => {
+                            return res.send({ status: "success", data: product, message: 'sản phẩm đã cập nhật lượt xem' });
+                        });
+                });
+        }
+
+    }
+    else {
+        return res.send({ status: "fail", message: 'key không hợp lệ' });
+    }
+}
+
 module.exports = {
-    getProduct: getProduct,
-    getProducts: getProducts
+    getProduct,
+    getProducts,
+    searchProducts,
+    updateView
 }

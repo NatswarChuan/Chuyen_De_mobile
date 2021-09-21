5264f51916e6d61b216db0343c563a02cd06e5b5
NatswarChuan <natswarchuan@gmail.com>
2021-09-21 13:15:15 +0700

    21/9 update link

--- api-image/app/config/config.js
+++ api-image/app/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     key: 'e4611a028c71342a5b083d2cbf59c494',
-    url: 'http://192.168.1.2:8080/fileimg/',
+    url: 'https://firebasestorage.googleapis.com/v0/b/alien-marking-317003.appspot.com/o/',
 }
